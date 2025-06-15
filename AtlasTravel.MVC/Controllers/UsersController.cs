ccb548866134c3a9357f2cd7598bfa1c396b3c47
Yuristam <yuristamasanakunov@gmail.com>
2025-06-16 00:56:11 +0600

    ADDED: index in UsersController

---
+++ AtlasTravel.MVC/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using AtlasTravel.MVC.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AtlasTravel.MVC.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly IUsersRepository _userRepository;
+
+        public UsersController(IUsersRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userRepository.GetAllUsersAsync();
+
+            return View(users);
+        }
+    }
+}
