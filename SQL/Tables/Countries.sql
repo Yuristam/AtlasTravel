4fca498502e7d9f06aec3c19478b190e29b5eb7b
Yuristam <yuristamasanakunov@gmail.com>
2025-06-21 11:31:25 +0600

    EDITED: added unique keys to users(email) and Countries (name)

--- SQL/Tables/Countries.sql
+++ SQL/Tables/Countries.sql
@@ -2,5 +2,6 @@ CREATE TABLE Countries (
 	CountryID TINYINT IDENTITY(1, 1),
 	CountryName NVARCHAR(250) NOT NULL,
 
+	CONSTRAINT UQ_Countries_Name UNIQUE (CountryName),
 	CONSTRAINT PK_Countries PRIMARY KEY (CountryID)
 )
